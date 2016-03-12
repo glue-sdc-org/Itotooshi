714e027aee119c6f6f32691cf93dcf328ba7a842
MegumiJ <megumi.jinu@gmail.com>
2016-03-12 15:43:53 +0900

    記録画面への画面遷移を追加

--- app/src/main/java/com/example/wakanushi/itotooshi/MainActivity.java
+++ app/src/main/java/com/example/wakanushi/itotooshi/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.wakanushi.itotooshi;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.record_button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RecordActivity.class));
+            }
+        });
     }
 }
